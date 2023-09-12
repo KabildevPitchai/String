7183c68753f5ab45a8a3c21aadb45462c3540dbc
KabildevPitchai <137326813+KabildevPitchai@users.noreply.github.com>
2023-09-12 13:31:04 +0530

    Add files via upload
    string

---
+++ Letter_printing_using_String.java
@@ -0,0 +1,39 @@
+package String;
+
+public class Letter_printing_using_String {
+
+	public static void main(String[] args) {
+		Letter_printing_using_String l=new Letter_printing_using_String();
+		l.Letter_printing();
+		l.letter_printing_Anothermethod();
+	}
+
+	private void letter_printing_Anothermethod() {
+		String s="A6B4";
+	
+		for (int i = 0; i < s.length()-1; i+=2) {
+			  
+			  char first = s.charAt(i);
+			  
+			  char num = s.charAt(i+1);
+			  
+			  for (int ch = '1'; ch <=num; ch++) 
+			  {
+			    System.out.print(first+" ");
+			  }
+		}
+		
+	}
+
+	private void Letter_printing() {
+		String s="A6B4";
+			char letter=s.charAt(0);
+			char num=s.charAt(1);
+			for(char ch='1';ch<=num;ch++) {
+				System.out.print(letter+" ");
+			}
+			System.out.println();
+		}
+	}
+
+
